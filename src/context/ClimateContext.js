5b775d14a97561300c94669b3ac3d15a2c73f8c0
Dalton Klossman <klossdal@gmail.com>
2021-06-10 15:44:17 -0400

    phase 3 complete

--- src/context/ClimateContext.js
+++ src/context/ClimateContext.js
@@ -1,2 +1,20 @@
+import { createContext, useState, useContext } from "react"
+
+export const ClimateContext = createContext()
+
+export const useClimate = () => useContext(ClimateContext)
+
+export const ClimateProvider = (props) => {
+  const [temperature, setTemperature] = useState(50)
+
+  return (
+    <ClimateContext.Provider value={{temperature, setTemperature}}>
+      {props.children}
+    </ClimateContext.Provider>
+  )
+}
+
+
+
 // Temperature has a default value of 50 degrees
 // Humidity has a default value of 40%
